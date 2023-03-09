f885e161c054f81179a3674dc7c6acb7c37e1a23
Phan Thị Quỳnh Như <82049190+NHU0606@users.noreply.github.com>
2023-03-09 11:52:05 +0700

    all src

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#top-message").load(`pages/layout/topMess.html`)
+    $("#header").load(`pages/layout/header.html`); 
+    $("#menu").load(`pages/menu.html`);
+    $("#slider").load(`pages/slider.html`); 
+    $("#content-sub").load(`pages/viral.html`); 
+    $("#footer").load(`pages/layout/footer.html`); 
+  });
+  
